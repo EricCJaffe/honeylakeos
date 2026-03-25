0b011eb4994d961b8a7891a4b1069095227e0933
EricCJaffe <ejaffejax@gmail.com>
2026-03-25 17:19:55 -0400

    fix: add red button variant and implement on dashboard

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const Dashboard = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <Button variant="red">red button</Button>
+    </div>
+  );
+};
+
+export default Dashboard;
